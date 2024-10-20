a8a641ddab9ed6b426a219694eeb69874940f688
Marco Antonio de Oliveira Filho <marco.oliveira92@hotmail.com>
2024-10-20 12:11:59 -0300

    primeiro contrato sem funcoes

---
+++ src/PersonDataStorage.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MITT
+pragma solidity ^0.8.13;
+
+contract Counter {
+    string public name = "Marco Antonio";
+    uint private age = 32;
+    bool internal brazilian = true;
+}
