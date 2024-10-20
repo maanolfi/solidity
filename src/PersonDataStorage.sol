6e6b426870e9d088d20dff37fa37210af6517798
Marco Antonio de Oliveira Filho <marco.oliveira92@hotmail.com>
2024-10-20 12:15:53 -0300

    correcao da variavel de inteiro

--- src/PersonDataStorage.sol
+++ src/PersonDataStorage.sol
@@ -3,6 +3,6 @@ pragma solidity ^0.8.13;
 
 contract Counter {
     string public name = "Marco Antonio";
-    uint private age = 32;
+    uint256 private age = 32;
     bool internal brazilian = true;
 }
